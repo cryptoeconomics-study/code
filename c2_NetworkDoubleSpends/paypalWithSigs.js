69d6f0b555dc4e1a3408d35f57c9b9ef242f66d6
Karl Floersch <karl@karlfloersch.com>
2018-06-17 20:29:31 -0400

    Implement network sim with random transactions

--- c2_NetworkDoubleSpends/paypalWithSigs.js
+++ c2_NetworkDoubleSpends/paypalWithSigs.js
@@ -1,89 +1,16 @@
 var EthCrypto = require('eth-crypto')
 var network = require('./networksim')()
 
-/*
- * Example Transactions
-
-var unsignedTxs = [
-  {
-    type: 'mint',
-    amount: 100,
-    from: nodes.paypal.address,
-    to: nodes.paypal.address,
-    nonce: 0
-  },
-  {
-    type: 'send',
-    amount: 65,
-    from: nodes.paypal.address,
-    to: nodes.aparna.address,
-    nonce: 1
-  },
-  {
-    type: 'send',
-    amount: 10,
-    from: nodes.aparna.address,
-    to: nodes.jing.address,
-    nonce: 0
-  }
-]
-
-var signedTxs = [
-  {
-    contents: unsignedTxs[0],
-    sig: EthCrypto.sign(nodes.paypal.privateKey, getTxHash(unsignedTxs[0]))
-  },
-  {
-    contents: unsignedTxs[1],
-    sig: EthCrypto.sign(nodes.paypal.privateKey, getTxHash(unsignedTxs[1]))
-  },
-  {
-    contents: unsignedTxs[2],
-    sig: EthCrypto.sign(nodes.aparna.privateKey, getTxHash(unsignedTxs[2]))
-  }
-]
-*/
-
 function getTxHash (tx) {
   return EthCrypto.hash.keccak256(JSON.stringify(tx))
 }
 
-class InvalidNonce extends Error {
-  constructor (...args) {
-    super(...args)
-    Error.captureStackTrace(this, InvalidNonce)
-  }
-}
-
-function applyTransaction (state, tx) {
-  // Check the from address matches the signature
-  const signer = EthCrypto.recover(tx.sig, getTxHash(tx.contents))
-  if (signer !== tx.contents.from) {
-    throw new Error('Invalid signature!')
-  }
-  // If we don't have a record for this address, create one
-  if (!(tx.contents.to in state)) {
-    state[[tx.contents.to]] = {
-      balance: 0,
-      nonce: -1
-    }
-  }
-  // Check that the nonce is correct for replay protection
-  if (tx.contents.nonce !== state[[tx.contents.from]].nonce + 1) {
-    throw new InvalidNonce(tx)
-  }
-  // Mint coins **only if identity is PayPal**
-  if (tx.contents.type === 'send') { // Send coins
-    if (state[[tx.contents.from]].balance - tx.contents.amount < 0) {
-      throw new Error('Not enough money!')
-    }
-    state[[tx.contents.from]].balance -= tx.contents.amount
-    state[[tx.contents.to]].balance += tx.contents.amount
-  } else {
-    throw new Error('Invalid transaction type!')
-  }
-  state[[tx.contents.from]].nonce += 1
-}
+// class InvalidNonce extends Error {
+//   constructor (...args) {
+//     super(...args)
+//     Error.captureStackTrace(this, InvalidNonce)
+//   }
+// }
 
 class Node {
   constructor (wallet, genesis, network) {
@@ -99,39 +26,19 @@ class Node {
   }
 
   onReceive (tx) {
-    if (!this.transactions.includes(tx)) {
-      this.transactions.push(tx)
-      this.network.broadcast(this.pid, tx)
+    if (this.transactions.includes(tx)) {
+      return
     }
-    try {
-      applyTransaction(this.state, tx)
-    } catch (e) {
-      // Catch invalid nonce
-      if (!(e instanceof InvalidNonce)) {
-        throw e
-      }
-      // Add transaction to invalidNonceTxs
-      if (!(tx.contents.from in this.invalidNonceTxs)) {
-        this.invalidNonceTxs[tx.contents.from] = {}
-      }
-      this.invalidNonceTxs[tx.contents.from][tx.contents.nonce] = tx
-    }
+    this.transactions.push(tx)
+    this.applyTransaction(tx)
+    this.network.broadcast(this.pid, tx)
     this.applyInvalidNonceTxs(tx.contents.from)
   }
 
   applyInvalidNonceTxs (address) {
-    const targetNonce = this.state[address].nonce + 1
+    const targetNonce = this.state[address].nonce
     if (address in this.invalidNonceTxs && targetNonce in this.invalidNonceTxs[address]) {
-      applyTransaction(this.state, this.invalidNonceTxs[address][targetNonce])
-      if (targetNonce > 3 && '2' in this.invalidNonceTxs[address]) {
-        console.log('my adddress:', address)
-        console.log("Deleting:", this.invalidNonceTxs[address][targetNonce])
-        console.log('~~~~~~~Transactions~~~~~~')
-        console.log(this.transactions)
-        console.log('~~~~~~~Invalid nonce txs~~~~~~')
-        console.log(this.invalidNonceTxs[address])
-        throw 'WTF'
-      }
+      this.applyTransaction(this.invalidNonceTxs[address][targetNonce])
       delete this.invalidNonceTxs[address][targetNonce]
       this.applyInvalidNonceTxs(address)
     }
@@ -144,16 +51,50 @@ class Node {
       amount: 1,
       from: this.wallet.address,
       to: nodes[Math.floor(Math.random() * nodes.length)].wallet.address,
-      nonce: this.state[this.wallet.address].nonce + 1
+      nonce: this.state[this.wallet.address].nonce
     }
     const tx = {
       contents: unsignedTx,
       sig: EthCrypto.sign(this.wallet.privateKey, getTxHash(unsignedTx))
     }
     this.transactions.push(tx)
-    applyTransaction(this.state, tx)
+    this.applyTransaction(tx)
     // Broadcast this tx to the network
     this.network.broadcast(this.pid, tx)
+  }
+
+  applyTransaction (tx) {
+    // Check the from address matches the signature
+    const signer = EthCrypto.recover(tx.sig, getTxHash(tx.contents))
+    if (signer !== tx.contents.from) {
+      throw new Error('Invalid signature!')
+    }
+    // If we don't have a record for this address, create one
+    if (!(tx.contents.to in this.state)) {
+      this.state[[tx.contents.to]] = {
+        balance: 0,
+        nonce: 0
+      }
+    }
+    // Check that the nonce is correct for replay protection
+    if (tx.contents.nonce !== this.state[[tx.contents.from]].nonce) {
+      // If it isn't correct, then we should add it to transaction to invalidNonceTxs
+      if (!(tx.contents.from in this.invalidNonceTxs)) {
+        this.invalidNonceTxs[tx.contents.from] = {}
+      }
+      this.invalidNonceTxs[tx.contents.from][tx.contents.nonce] = tx
+      return
+    }
+    if (tx.contents.type === 'send') { // Send coins
+      if (this.state[[tx.contents.from]].balance - tx.contents.amount < 0) {
+        throw new Error('Not enough money!')
+      }
+      this.state[[tx.contents.from]].balance -= tx.contents.amount
+      this.state[[tx.contents.to]].balance += tx.contents.amount
+    } else {
+      throw new Error('Invalid transaction type!')
+    }
+    this.state[[tx.contents.from]].nonce += 1
   }
 }
 
@@ -184,7 +125,7 @@ console.log(nodes[0].state)
 console.log('~~~~~~~~~~~ Node 1 ~~~~~~~~~~~')
 console.log(nodes[1].state)
 console.log('~~~~~~~~~~~ Node 1 ~~~~~~~~~~~')
-console.log(nodes[1].invalidNonceTxs)
+console.log(nodes[1].invalidNonceTxs[wallets[0].address])
 
 // for (tx of nodes[1].transactions) {
 //   if (tx.contents.from == nodes[2].wallet.address) {
