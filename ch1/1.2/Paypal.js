a0c8d347c20112902ccaf8e14ba84741356c9f11
burrrata <42473928+burrrata@users.noreply.github.com>
2019-07-26 16:11:40 -0400

    updated PayPayl.js w code comments

--- ch1/1.2/Paypal.js
+++ ch1/1.2/Paypal.js
@@ -1,15 +1,34 @@
 const EthCrypto = require('eth-crypto')
 const Client = require('./Client.js')
 
+// Our naive implimentation of a centralized payment processor
 class Paypal extends Client {
     constructor(genesis) {
         super()
+        // the state of the network (accounts and balances)
         this.state = {
             [this.wallet.address]: {
                 balance: 0
             }
         }
+        // the history of transactions
         this.transactions = []
+        // a pool of pending transactoins
+        this.pendingTransactions = []
+    }
+
+    // Generates new transactions
+    function generateTx(to, amount, type) {
+        // TODO:
+        // - create an unsigned transaction
+        // - create a digital signature of the transaction
+        // - return a Javascript object with the unsigned transaction and transaction signature
+    }
+
+    // Processes transactions and updates the state
+    function stateTransitionFunction(transaction) {
+        // TODO: apply transaction to this.state
+        // Note: in the future this is where we'll apply protocol rules to check the transaction and reject those that are invalid, but right now we're just going to process all transactions as valid
     }
 }
 
