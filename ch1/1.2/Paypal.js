7694c823654c247e14bbd742c8bf13df727fe6b5
burrrata <42473928+burrrata@users.noreply.github.com>
2019-07-26 20:04:28 -0400

    major refactor

--- ch1/1.2/Paypal.js
+++ ch1/1.2/Paypal.js
@@ -12,15 +12,50 @@ class Paypal extends Client {
             }
         }
         // the history of transactions
-        this.transactions = []
+        this.transactionHistory = []
     }
 
-    // Receives a transaction, checks if it's valid, and updates the state
-    function stateTransitionFunction(transaction) {
-        // TODO: 
-			  // - if a transaction is minting new tokens, check that the sender is the central payment operator (PayPal)
-        // - if a transaction is moving tokens from one wallet to another, check that the sender's account balance is greater than or equal to the amount of the transaction
-        // - if all checks pass, process the transaction (updated the account balances accordingly and add the transaction to this.transactions) 
+	// Checks that the sender of a transaction is the same as the signer
+	checkTxSignature(tx) {
+	  // TODO: 
+	  // - check the signature of a tx and return a boolean
+	  // - return an error if the signature is invalid
+	  // - return true if the tx is valid
+	}
+
+	// Checks if the user's address is already in the state, and if not, adds the user's address to the state
+	checkUserAddress(tx) {
+	  // TODO:
+	  // - check if the sender is in the state
+	  // - check if the receiver is in the state
+	  // - if either the sender or the receiver is not in the state, add their address to the state, initialize the balance at 0, and loop back through the function
+	  // - if the checks on both accounts pass (they're both in the state), return true
+	}
+
+	// Checks the transaction type and ensures that the transaction is valid based on that type
+	checkTxType(tx) {
+	  // TODO:
+	  // - if mint, check that the sender is PayPal
+	  // - if send, check that the transaction amount is positive and the sender has an account balance greater than or equal to the transaction amount 
+	  // - if a check fails, return an error stating why
+	  // - if a check passes, return true
+	}
+
+	// Updates account balances according to a transaction and adds the transaction to the history
+	processTransaction(tx) {
+	  // TODO:
+	  // - decrease the balance of the transaction sender/signer
+	  // - increase the balance of the transaction receiver 
+	  // - add the transaction to the transaction history
+	}
+
+    // Checks if a transaction is valid, adds it to the transaction history, and updates the state of accounts and balances
+    stateTransitionFunction(transaction) {
+	  // TODO: 
+	  // - check that the transaction signature is valid
+	  // - check that the transaction sender and receiver are in the state
+	  // - check that the transaction type is valid
+	  // - process the transaction and add it to the transaction history
     }
 }
 
--- ch1/1.2/solution/Paypal.js
+++ ch1/1.2/solution/Paypal.js
@@ -1,51 +1,118 @@
 const EthCrypto = require('eth-crypto')
 const Client = require('./Client.js')
 
+// Our naive implimentation of a centralized payment processor
 class Paypal extends Client {
-    constructor() {
+    constructor(genesis) {
         super()
+        // the state of the network (accounts and balances)
         this.state = {
             [this.wallet.address]: {
                 balance: 0
             }
         }
-        this.transactions = []
+        // the history of transactions
+        this.transactionHistory = []
     }
 
-    applyTransaction(tx) {
-        // verify the signature
-        const validSig = this.verify (
-            tx.sig,
-            this.toHash(tx.contents),
-            tx.contents.from
-        )
-        if (!validSig) {
-            throw new Error('Invalid signature!')
-        }
-        // If we don't have a record for this address, create one
-        if (!(tx.contents.to in this.state)) {
-            this.state[tx.contents.to] = {
-                balance: 0
-            }
-        }
-        // Check that the nonce is correct for replay protection
-        //  if (tx.contents.nonce !== state[[tx.contents.from]].nonce + 1) {
-        //   throw new Error('Invalid nonce!')
-        // }
-        // Mint coins **only if identity is PayPal**
-        if (tx.contents.type === 'mint') {
-            if(tx.contents.from !== this.wallet.address) {
-                throw new Error('Non-Paypal Clients can\'t mint!')
-            }
-            this.state[tx.contents.to].balance += tx.contents.amount
-        } else if (tx.contents.type === 'send') { // Send coins
-            if (this.state[tx.contents.from].balance - tx.contents.amount < 0) {
-                throw new Error('Not enough money!')
-            }
-            this.state[tx.contents.from].balance -= tx.contents.amount
-            this.state[tx.contents.to].balance += tx.contents.amount
-        }
-        // state[[tx.contents.from]].nonce += 1
+	// Checks that the sender of a transaction is the same as the signer
+	checkTxSignature(tx) {
+	  // TODO: 
+	  // - check the signature of a tx and return a boolean
+	  // - return an error if the signature is invalid
+	  // - return true if the tx is valid
+	  
+	  // check the signature of a tx and return a boolean
+	  const sig = this.verify(tx.sig, this.toHash(tx.contents), tx.contents.from);
+	  // return an error if the signature is invalid
+	  if (!sig) {
+			throw new Error('Invalid Signature!');							
+		// return true if the tx is valid
+		} else {
+			return true;
+		}
+	}
+
+	// Checks if the user's address is already in the state, and if not, adds the user's address to the state
+	checkUserAddress(tx) {
+	  // TODO:
+	  // - check if the sender is in the state
+	  // - check if the receiver is in the state
+	  // - if either the sender or the receiver is not in the state, add their address to the state, initialize the balance at 0, and loop back through the function
+	  // - if the checks on both accounts pass (they're both in the state), return true
+
+	  // check if the sender is in the state
+	  if (!(tx.contents.to in this.state)) {
+		  this.state[tx.contents.to] = {
+			  balance: 0
+		  }
+	  }
+	  // check if the receiver is in the state
+	  if (!(tx.contents.from in this.state)) {
+		  this.state[tx.contents.to] = {
+			  balance: 0
+		  }
+	  }
+	  // if the checks on both accounts pass (they're both in the state), return true
+	  return true
+	}
+
+	// Checks the transaction type and ensures that the transaction is valid based on that type
+	checkTxType(tx) {
+	  // TODO:
+	  // - if mint, check that the sender is PayPal
+	  // - if send, check that the transaction amount is positive and the sender has an account balance greater than or equal to the transaction amount 
+	  // - if a check fails, return an error stating why
+	  // - if a check passes, return true
+
+	  // if mint, check that the sender is PayPal
+	  if (tx.contents.type === 'mint') {
+		  if(tx.contents.from !== this.wallet.address) {
+			  // if a check fails, return an error stating why
+			  throw new Error('Non-Paypal Clients can\'t mint!')
+		  }
+	  // if send, check that the transaction amount is positive and the sender has an account balance greater than or equal to the transaction amount 
+	  } else if (tx.contents.type === 'send') { // Send coins
+		  if (this.state[tx.contents.from].balance - tx.contents.amount < 0) {
+			  // if a check fails, return an error stating why
+			  throw new Error('Not enough money!')
+		  }
+	  }
+	  // if a check passes, return true
+	  return true
+	}
+
+	// Updates account balances according to a transaction and adds the transaction to the history
+	processTransaction(tx) {
+	  // TODO:
+	  // - decrease the balance of the transaction sender/signer
+	  // - increase the balance of the transaction receiver 
+	  // - add the transaction to the transaction history
+
+	  // decrease the balance of the transaction sender/signer
+	  this.state[tx.contents.from].balance -= tx.contents.amount
+	  // increase the balance of the transaction receiver 
+	  this.state[tx.contents.to].balance += tx.contents.amount
+	  // add the transaction to the transaction history
+	  this.transactionHistory.push(tx)
+	}
+
+    // Checks if a transaction is valid, adds it to the transaction history, and updates the state of accounts and balances
+    stateTransitionFunction(transaction) {
+	  // TODO: 
+	  // - check that the transaction signature is valid
+	  // - check that the transaction sender and receiver are in the state
+	  // - check that the transaction type is valid
+	  // - process the transaction and add it to the transaction history
+
+	  // check that the transaction signature is valid
+	  checkTxSignature(tx)
+	  // check that the transaction sender and receiver are in the state
+	  checkUserAddress(tx)
+	  // check that the transaction type is valid
+	  checkTxType(tx)
+	  // process the transaction and add it to the transaction history
+	  processTransaction(tx)
     }
 }
 
