dfdc9b674c0d7098ff05c093086eea17103962c5
burrrata <42473928+burrrata@users.noreply.github.com>
2019-08-10 21:40:53 -0400

    renamed ch2 sections

--- ch2/nodeAgent.js
+++ ch2/nodeAgent.js
@@ -1,91 +1,95 @@
-var EthCrypto = require('eth-crypto')
+const EthCrypto = require('eth-crypto');
 
-function getTxHash (tx) {
-  return EthCrypto.hash.keccak256(JSON.stringify(tx))
+function getTxHash(tx) {
+  return EthCrypto.hash.keccak256(JSON.stringify(tx));
 }
 
 class Node {
-  constructor (wallet, genesis, network) {
+  constructor(wallet, genesis, network) {
     // Blockchain identity
-    this.wallet = wallet
+    this.wallet = wallet;
     // P2P Node identity -- used for connecting to peers
-    this.p2pNodeId = EthCrypto.createIdentity()
-    this.pid = this.p2pNodeId.address
-    this.network = network
-    this.state = genesis
-    this.transactions = []
-    this.invalidNonceTxs = {}
+    this.p2pNodeId = EthCrypto.createIdentity();
+    this.pid = this.p2pNodeId.address;
+    this.network = network;
+    this.state = genesis;
+    this.transactions = [];
+    this.invalidNonceTxs = {};
   }
 
-  onReceive (tx) {
+  onReceive(tx) {
     if (this.transactions.includes(tx)) {
-      return
+      return;
     }
-    this.transactions.push(tx)
-    this.applyTransaction(tx)
-    this.network.broadcast(this.pid, tx)
-    this.applyInvalidNonceTxs(tx.contents.from)
+    this.transactions.push(tx);
+    this.applyTransaction(tx);
+    this.network.broadcast(this.pid, tx);
+    this.applyInvalidNonceTxs(tx.contents.from);
   }
 
-  applyInvalidNonceTxs (address) {
-    const targetNonce = this.state[address].nonce
-    if (address in this.invalidNonceTxs && targetNonce in this.invalidNonceTxs[address]) {
-      this.applyTransaction(this.invalidNonceTxs[address][targetNonce])
-      delete this.invalidNonceTxs[address][targetNonce]
-      this.applyInvalidNonceTxs(address)
+  applyInvalidNonceTxs(address) {
+    const targetNonce = this.state[address].nonce;
+    if (
+      address in this.invalidNonceTxs
+      && targetNonce in this.invalidNonceTxs[address]
+    ) {
+      this.applyTransaction(this.invalidNonceTxs[address][targetNonce]);
+      delete this.invalidNonceTxs[address][targetNonce];
+      this.applyInvalidNonceTxs(address);
     }
   }
 
-  tick () {}
+  tick() {}
 
-  generateTx (to, amount) {
+  generateTx(to, amount) {
     const unsignedTx = {
       type: 'send',
-      amount: amount,
+      amount,
       from: this.wallet.address,
-      to: to,
-      nonce: this.state[this.wallet.address].nonce
-    }
+      to,
+      nonce: this.state[this.wallet.address].nonce,
+    };
     const tx = {
       contents: unsignedTx,
-      sig: EthCrypto.sign(this.wallet.privateKey, getTxHash(unsignedTx))
-    }
-    return tx
+      sig: EthCrypto.sign(this.wallet.privateKey, getTxHash(unsignedTx)),
+    };
+    return tx;
   }
 
-  applyTransaction (tx) {
+  applyTransaction(tx) {
     // Check the from address matches the signature
-    const signer = EthCrypto.recover(tx.sig, getTxHash(tx.contents))
+    const signer = EthCrypto.recover(tx.sig, getTxHash(tx.contents));
     if (signer !== tx.contents.from) {
-      throw new Error('Invalid signature!')
+      throw new Error('Invalid signature!');
     }
     // If we don't have a record for this address, create one
     if (!(tx.contents.to in this.state)) {
       this.state[[tx.contents.to]] = {
         balance: 0,
-        nonce: 0
-      }
+        nonce: 0,
+      };
     }
     // Check that the nonce is correct for replay protection
     if (tx.contents.nonce !== this.state[[tx.contents.from]].nonce) {
       // If it isn't correct, then we should add the transaction to invalidNonceTxs
       if (!(tx.contents.from in this.invalidNonceTxs)) {
-        this.invalidNonceTxs[tx.contents.from] = {}
+        this.invalidNonceTxs[tx.contents.from] = {};
       }
-      this.invalidNonceTxs[tx.contents.from][tx.contents.nonce] = tx
-      return
+      this.invalidNonceTxs[tx.contents.from][tx.contents.nonce] = tx;
+      return;
     }
-    if (tx.contents.type === 'send') { // Send coins
+    if (tx.contents.type === 'send') {
+      // Send coins
       if (this.state[[tx.contents.from]].balance - tx.contents.amount < 0) {
-        throw new Error('Not enough money!')
+        throw new Error('Not enough money!');
       }
-      this.state[[tx.contents.from]].balance -= tx.contents.amount
-      this.state[[tx.contents.to]].balance += tx.contents.amount
+      this.state[[tx.contents.from]].balance -= tx.contents.amount;
+      this.state[[tx.contents.to]].balance += tx.contents.amount;
     } else {
-      throw new Error('Invalid transaction type!')
+      throw new Error('Invalid transaction type!');
     }
-    this.state[[tx.contents.from]].nonce += 1
+    this.state[[tx.contents.from]].nonce += 1;
   }
 }
 
-module.exports = {Node, getTxHash}
+module.exports = { Node, getTxHash };
