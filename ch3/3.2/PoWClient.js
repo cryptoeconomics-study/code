a6c623f23280a9e8f46c713829a8aeb28dc7c19d
burrrata <42473928+burrrata@users.noreply.github.com>
2019-09-03 22:56:49 -0400

    added hex-to-binary as npm dependency in package.json

--- ch3/3.2/PoWClient.js
+++ ch3/3.2/PoWClient.js
@@ -1,15 +1,15 @@
-var EthCrypto = require('eth-crypto')
-var hexToBinary = require('hex-to-binary')
-var {Node, getTxHash} = require('../nodeAgent')
+const EthCrypto = require('eth-crypto');
+const hexToBinary = require('hex-to-binary');
+const { Node, getTxHash } = require('../nodeAgent');
 
 // Add Client class which generates & sends transactions
 class Client extends Node {
-  constructor (wallet, genesis, network) {
-    super(wallet, genesis, network)
-    this.blockchain = [] //longest chain
-    this.allBlocks = []  //all blocks
-    this.blockNumber = 0 //keep track of blocks added to blockchain despite getState()
-    this.difficulty = 13
+  constructor(wallet, genesis, network) {
+    super(wallet, genesis, network);
+    this.blockchain = []; // longest chain
+    this.allBlocks = []; // all blocks
+    this.blockNumber = 0; // keep track of blocks added to blockchain despite getState()
+    this.difficulty = 13;
 
     const genesisBlock = {
       nonce: 0,
@@ -20,101 +20,105 @@ class Client extends Node {
       timestamp: 0,
       contents: {
         type: 'block',
-        txList: []
-      }
-    }
-    this.blockchain.push(genesisBlock)
-    this.allBlocks.push(genesisBlock)
+        txList: [],
+      },
+    };
+    this.blockchain.push(genesisBlock);
+    this.allBlocks.push(genesisBlock);
   }
 
   onReceive(message) {
-    switch(message.contents.type) {
+    switch (message.contents.type) {
       case 'send':
-        this.receiveTx(message)
-        break
+        this.receiveTx(message);
+        break;
       case 'block':
-        this.receiveBlock(message)
-        break
+        this.receiveBlock(message);
+        break;
     }
   }
 
   receiveTx(tx) {
-    if (this.transactions.includes(tx)) return
-    this.transactions.push(tx)      //add tx to mempool
-    this.network.broadcast(this.pid, tx)
+    if (this.transactions.includes(tx)) return;
+    this.transactions.push(tx); // add tx to mempool
+    this.network.broadcast(this.pid, tx);
   }
 
   isValidBlockHash(block) {
-    const blockHash = getTxHash(block)
-    //Found block
-    const binBlockHash = hexToBinary(blockHash.substr(2))
-    const leadingZeros = parseInt(binBlockHash.substring(0, this.difficulty))
-    return (leadingZeros === 0)
+    const blockHash = getTxHash(block);
+    // Found block
+    const binBlockHash = hexToBinary(blockHash.substr(2));
+    const leadingZeros = parseInt(binBlockHash.substring(0, this.difficulty));
+    return leadingZeros === 0;
   }
 
   receiveBlock(block) {
-    if (this.allBlocks.includes(block))
-      return
-    if (!this.isValidBlockHash(block))
-      return
-    this.allBlocks.push(block) //add block to all blocks received
-    //if the block builds directly on the current head of the chain, append to chain
+    if (this.allBlocks.includes(block)) return;
+    if (!this.isValidBlockHash(block)) return;
+    this.allBlocks.push(block); // add block to all blocks received
+    // if the block builds directly on the current head of the chain, append to chain
     if (block.parentHash === getTxHash(this.blockchain.slice(-1)[0])) {
-      this.blockNumber++ //increment
-      this.blockchain.push(block) //add block to own blockchain
-      this.applyBlock(block)
+      this.blockNumber++; // increment
+      this.blockchain.push(block); // add block to own blockchain
+      this.applyBlock(block);
     } else {
-      this.allBlocks.push(block)
-      this.updateState() //check if blockchain is the longest sequence
+      this.allBlocks.push(block);
+      this.updateState(); // check if blockchain is the longest sequence
     }
-    this.network.broadcast(this.pid, block) //broadcast new block to network
+    this.network.broadcast(this.pid, block); // broadcast new block to network
   }
 
   // Fork choice
   // Only apply transactions which are contained in the longest chain
   // and returns the resulting state object.
   updateState() {
-    //a temp chain
-    let tempChain = []
-    let allBlocks = this.allBlocks
-    //return max blocknumber from allBlocks
-    let max = Math.max.apply(Math, allBlocks.map(function(block) { return block.number; }))
-    //add the highestBlockNumber to tempChain using blockNumber
-    for (let block of allBlocks) { //TODO optimize this...Once there are many blocks in allBlocks, this may be SLOW af
+    // a temp chain
+    const tempChain = [];
+    const { allBlocks } = this;
+    // return max blocknumber from allBlocks
+    const max = Math.max.apply(
+      Math,
+      allBlocks.map(block => block.number),
+    );
+    // add the highestBlockNumber to tempChain using blockNumber
+    for (const block of allBlocks) {
+      // TODO optimize this...Once there are many blocks in allBlocks, this may be SLOW af
       if (block.number === max) {
-        tempChain.push(block)
+        tempChain.push(block);
         break;
       }
     }
-    //add max number of blocks to tempChain using parentHash
-    //i is the current block number
+    // add max number of blocks to tempChain using parentHash
+    // i is the current block number
     for (let i = max; i > 0; i--) {
-      const prevHash = tempChain[0].parentHash
-      for (let block of allBlocks) {
-        if (getTxHash(block) === prevHash) { //TODO verify blockhash before adding to allBlocks
-          tempChain.unshift(block) //add block to front of array
+      const prevHash = tempChain[0].parentHash;
+      for (const block of allBlocks) {
+        if (getTxHash(block) === prevHash) {
+          // TODO verify blockhash before adding to allBlocks
+          tempChain.unshift(block); // add block to front of array
           break;
         }
       }
     }
-    //save the ordered sequence
-    this.blockchain = tempChain
-    //apply all txs from ordered list of blocks
-    for (let block of this.blockchain) {
-      this.applyBlock(block)
+    // save the ordered sequence
+    this.blockchain = tempChain;
+    // apply all txs from ordered list of blocks
+    for (const block of this.blockchain) {
+      this.applyBlock(block);
     }
-    return this.state
+    return this.state;
   }
 
   applyBlock(block) {
-    const txList = block.contents.txList
-    for (let tx of txList) {
-      this.applyTransaction(tx)       //update state
-      if (tx.contents.from !== 0) { //mint tx is excluded
-        this.applyInvalidNonceTxs(tx.contents.from) //update state
+    const { txList } = block.contents;
+    for (const tx of txList) {
+      this.applyTransaction(tx); // update state
+      if (tx.contents.from !== 0) {
+        // mint tx is excluded
+        this.applyInvalidNonceTxs(tx.contents.from); // update state
       }
     }
   }
 }
 
-module.exports = Client
+module.exports = Client;
--- ch3/3.2/PoWMiner.js
+++ ch3/3.2/PoWMiner.js
@@ -1,56 +1,60 @@
-var EthCrypto = require('eth-crypto')
-var Client = require('./PoWClient')
-var {getTxHash} = require('../nodeAgent')
+var EthCrypto = require("eth-crypto");
+var Client = require("./PoWClient");
+var { getTxHash } = require("../nodeAgent");
 
 class Miner extends Client {
-
-  constructor (wallet, genesis, network) {
-    super(wallet, genesis, network)
-    this.blockAttempt = this.createBlock()
-    this.hashRate = 5   // hashRate = # hashes miner can try at each tick
+  constructor(wallet, genesis, network) {
+    super(wallet, genesis, network);
+    this.blockAttempt = this.createBlock();
+    this.hashRate = 5; // hashRate = # hashes miner can try at each tick
   }
   tick() {
     //Mining
     for (let i = 0; i < this.hashRate; i++) {
       if (this.isValidBlockHash(this.blockAttempt)) {
-        const blockHash = getTxHash(this.blockAttempt)
-        console.log(this.pid.substring(0, 6), 'found a block:', blockHash.substring(0, 10),'at height', this.blockAttempt.number)
-        const validBlock = this.blockAttempt
-        this.receiveBlock(validBlock)
-        return
-      }
-      else this.blockAttempt.nonce++
+        const blockHash = getTxHash(this.blockAttempt);
+        console.log(
+          this.pid.substring(0, 6),
+          "found a block:",
+          blockHash.substring(0, 10),
+          "at height",
+          this.blockAttempt.number
+        );
+        const validBlock = this.blockAttempt;
+        this.receiveBlock(validBlock);
+        return;
+      } else this.blockAttempt.nonce++;
     }
   }
-  receiveBlock(block){
-    const parentHash = this.blockAttempt.parentHash
-    super.receiveBlock(block)
-    const newHead = this.blockchain.slice(-1)[0]
+  receiveBlock(block) {
+    const parentHash = this.blockAttempt.parentHash;
+    super.receiveBlock(block);
+    const newHead = this.blockchain.slice(-1)[0];
     //if the block head has changed, mine on top of the new head
-    if(getTxHash(newHead) !== parentHash) {
-      this.blockAttempt = this.createBlock() //Start mining new block
+    if (getTxHash(newHead) !== parentHash) {
+      this.blockAttempt = this.createBlock(); //Start mining new block
     }
   }
   createBlock() {
-    const tx = this.transactions.shift()
-    let txList = []
-    if (tx) txList.push(tx)
+    const tx = this.transactions.shift();
+    let txList = [];
+    if (tx) txList.push(tx);
 
-    let timestamp = Math.round(new Date().getTime() / 1000)
+    let timestamp = Math.round(new Date().getTime() / 1000);
     const newBlock = {
       nonce: 0,
-      number: this.blockNumber+1,
+      number: this.blockNumber + 1,
       coinbase: this.wallet.address,
       difficulty: this.difficulty,
       parentHash: getTxHash(this.blockchain.slice(-1)[0]),
       timestamp: timestamp,
       contents: {
-        type: 'block',
+        type: "block",
         txList
       }
-    }
-    return newBlock
+    };
+    return newBlock;
   }
 }
 
-module.exports = Miner
+module.exports = Miner;
